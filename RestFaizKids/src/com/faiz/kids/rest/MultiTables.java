99eda126f6906b571e22216b2b0c1233b788e568
faizshaik16 <faizshaik16@gmail.com>
2015-05-30 17:00:59 -0400

    add2num

--- RestFaizKids/src/com/faiz/kids/rest/MultiTables.java
+++ RestFaizKids/src/com/faiz/kids/rest/MultiTables.java
@@ -30,7 +30,20 @@ public class MultiTables {
 	public String getTable( @QueryParam("number") int number){
 		
 		////http://localhost:8080/RestFaizKids/api/faiz/kids/Table?number=2
+		//http://192.168.1.103:8080/RestFaizKids/api/faiz/kids/Table?number=25
 		return buildHTMLTable("Table for " + number, "Multiplacation Table" , number);
+	}
+	
+	
+	@GET
+	@Path("/add")
+	@Produces (MediaType.TEXT_HTML)
+	public String addTwoNumber( @QueryParam("param1") int param1, 
+			@QueryParam("param2") int param2){
+		
+		////http://localhost:8080/RestFaizKids/api/faiz/kids/add?param1=25&param2=25
+		//http://192.168.1.103:8080/RestFaizKids/api/faiz/kids/add?param1=25&param2=25
+		return buildHTMLAdd("add two number " + param1 + " + " + param2, "add two" , param1, param2);
 	}
 	
 	private String buildHTMLTable(String message, String title, int num){
@@ -56,12 +69,50 @@ public class MultiTables {
 		sb.append("<table>");
 		
 		
-		for(int i =1; i <11; i++){
+		for(int i =1; i <101; i++){
 			sb.append("<tr><td>");
 			String line = num + "  *  " + i + "  =  " + i*num;
 			sb.append(line);
 			sb.append("</td></tr>");
 		}
+		sb.append("</table>");
+		
+		sb.append("</body>");
+		sb.append("</html");
+		
+		return sb.toString();
+	}
+	
+	
+private String buildHTMLAdd(String message, String title, int num, int num2){
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("<!DOCTYPE html>");
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title>");
+		sb.append(title);
+		sb.append("</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		
+		sb.append("<h1>");
+		sb.append(title);
+		sb.append("</h1>");
+		
+		sb.append("<p>");
+		sb.append(message);
+		sb.append("</p>");
+		
+		sb.append("<table>");
+		
+		
+		
+			sb.append("<tr><td>");
+			String line = num + "  +  " + num2 + "  =  " + (num+num2);
+			sb.append(line);
+			sb.append("</td></tr>");
+
 		sb.append("</table>");
 		
 		sb.append("</body>");
